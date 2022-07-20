d4b4260f2deda145de5a0df83fbf34fdbd761b43
fakhrulzulfi <fakhrulzulfi@gmail.com>
2022-07-20 14:11:25 +0700

    Added Signin feature and JWT auth

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const Player = require('../player/model');
 const path = require('path');
 const fs = require('fs');
 const config = require('../../config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     signUp: async (req, res, next) => {
@@ -56,6 +58,50 @@ module.exports = {
             if( error && error.name === 'ValidationError' ) {
                 return res.status(422).json({
                     error: 1,
+                    message: 'Terjadi kesalahan pada server', 
+                    fields: error.errors
+                });
+            }
+            next(error);
+        }
+    },
+    signIn: async (req, res, next) => {
+        try {
+            const { email, password } = req.body;
+
+            const player = await Player.findOne({ email });
+
+            if( !player ) {
+                return res.status(401).json({
+                    message: 'Email yang Anda masukkan tidak terdaftar'
+                });
+            }
+
+            const matchPassword = bcrypt.compareSync(password, player.password);
+
+            if( !matchPassword ) {
+                return res.status(401).json({
+                    message: 'Kata sandi salah'
+                });
+            }
+
+            const token = jwt.sign({
+                player: {
+                    id: player.id,
+                    username: player.username,
+                    email: player.email,
+                    name: player.name,
+                    phoneNumber: player.phoneNumber,
+                    avatar: player.avatar
+                }
+            }, config.jwtKey);
+
+            return res.status(200).json({
+                data: { token }
+            });
+        } catch (error) {
+            if( error && error.name === 'ValidationError' ) {
+                return res.status(422).json({
                     message: 'Terjadi kesalahan pada server', 
                     fields: error.errors
                 });
--- app/auth/router.js
+++ app/auth/router.js
@@ -3,8 +3,10 @@ const multer = require('multer');
 const os = require('os');
 const {
     signUp,
+    signIn,
 } = require('./controller');
 
 router.post('/signup', multer({dest: os.tmpdir()}).single('image'), signUp);
+router.post('/signin', signIn);
 
 module.exports = router;
